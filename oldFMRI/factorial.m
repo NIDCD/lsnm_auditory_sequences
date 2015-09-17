6bc54206340078cd98acd6a5651d40eaf4c74721
Antonio Ulloa <antonio.ulloa@alum.bu.edu>
2015-09-17 13:28:20 -0400

    Cleaned up all directories

--- oldFMRI/factorial.m
+++
@@ -1,5 +0,0 @@
-function f=factorial(n)
-% Would you believe factorial n?
-% n - vector
-f=cumprod(1:max(n));
-f=f(n);
--- oldFMRI/poisson.m
+++
@@ -1,9 +0,0 @@
-% poisson.m
-
-function h=poisson(lambda,tau)
-
-if tau~=0
-  h=((lambda)^tau)*(exp(-lambda))/factorial(tau);
-elseif tau==0
-  h=((lambda)^tau)*(exp(-lambda));
-end
